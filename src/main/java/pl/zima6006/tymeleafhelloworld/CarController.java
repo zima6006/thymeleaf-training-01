68a74473760bf2d6c25964cce54c6c306a5d7a32
Michał Grudzień <zima6006@gmail.com>
2021-05-16 09:27:37 +0200

    lekcja-02-iteracja-po-liscie

--- src/main/java/pl/zima6006/tymeleafhelloworld/CarController.java
+++ src/main/java/pl/zima6006/tymeleafhelloworld/CarController.java
@@ -4,15 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class CarController {
 
     @GetMapping("/car")
     public  String getCar(Model model){
-        Car car = new Car("Fiat","Abart");
+        Car car1 = new Car("Audi","80");
+        Car car2 = new Car("Bentley","super");
+        Car car3 = new Car("Citroen","C5");
+        Car car4 = new Car("Dacia","Doper");
+        Car car5 = new Car("Fiat","Abart");
 
-        model.addAttribute("name", "Michał");
-        model.addAttribute("car",car);
+        List<Car> cars =Arrays.asList(car1,car2,car3,car4,car5);
+
+        model.addAttribute("cars", cars);
+
+        for(Car car :cars){
+            System.out.println("a");
+        }
+
         return "car";
     }
 }
